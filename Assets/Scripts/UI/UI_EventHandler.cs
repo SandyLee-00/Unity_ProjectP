bf25be693521c25cb6b7c294551e3e588c9e8af9
Sandy <syjy8809@gmail.com>
2024-05-18 19:16:16 +0900

    [FEAT] UI 이벤트 핸들러 Click, Pressed, PointerDown, PointerUp

--- Assets/Scripts/UI/UI_EventHandler.cs
+++ Assets/Scripts/UI/UI_EventHandler.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
 
-public class UI_EventHandler : MonoBehaviour
+/// <summary>
+/// UI 이벤트 핸들러 Click, Pressed, PointerDown, PointerUp
+/// </summary>
+public class UI_EventHandler : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public Action OnClickHandler = null;
+
+    public Action OnPointerDownHandler = null;
+
+    bool _pressed = false;
+    public Action OnPressedHandler = null;
+
+    public Action OnPointerUpHandler = null;
+
+    public void OnPointerClick(PointerEventData eventData)
     {
-        
+        OnClickHandler?.Invoke();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerDown(PointerEventData eventData)
     {
-        
+        _pressed = true;
+        OnPointerDownHandler?.Invoke();
+    }
+
+    private void Update()
+    {
+        if (_pressed)
+            OnPressedHandler?.Invoke();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _pressed = false;
+        OnPointerUpHandler?.Invoke();
     }
 }
